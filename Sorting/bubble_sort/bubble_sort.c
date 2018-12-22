a1e9d7da4c8cffd8948082fd3ad14ea787ccfdf9
jtristao <joaovitortristao@gmail.com>
2018-12-22 10:55:27 -0200

    Add bubble sort

--- Sorting/bubble_sort/bubble_sort.c
+++ Sorting/bubble_sort/bubble_sort.c
@@ -8,8 +8,27 @@ void swap(int *vector, int i, int j){
 
 void bubble_sort(int *vector, int n){
 	for(int i = 0; i < n-1; i++){
-		for(int j = i; j <=n; j++)
+		for(int j = i; j <= n; j++){
+			/* Compare function */
 			if(vector[i] > vector[j])
 				swap(vector, i, j);
+		}
 	}
 }
+
+/* Omega(n) Best case implementation
+/*void bubble_sort(int *vector, int n){
+	int new_n;
+
+	while(n >= 1){
+		new_n = 0;
+		for(int i = 1; i <= n-1; i++){
+			if(vector[i-1] > vector[i]){
+				swap(vector, i-1, i);
+				new_n = i;
+			}
+		}
+		n = new_n;
+	}
+}
+*/
--- Sorting/bubble_sort/test.c
+++ Sorting/bubble_sort/test.c
@@ -2,31 +2,68 @@
 #include <stdlib.h>
 
 #include "vector.h"
-#include "bubble_sort.h"
 
 enum{
 	PROGNAME,
 	N,
+	ORDER,
 	NARGS	
 };
 
+int swaps = 0;
+int comp = 0;
+
+void swap(int *vector, int i, int j){
+	int aux = vector[i];
+	vector[i] = vector[j];
+	vector[j] = aux;
+}
+
+void bubble_sort(int *vector, int n){
+	for(int i = 0; i < n-1; i++){
+		for(int j = i; j <= n; j++){
+			/* Compare function */
+			if(vector[i] > vector[j]){
+				swaps++;
+				swap(vector, i, j);
+			}
+			comp++;
+		}
+	}
+}
+
 int main(int argc, char *argv[]){
-	int n;
+	int *vector;
+	int n, order;
 
 	if(argc != NARGS){
-		printf("usage %s: N\n", argv[PROGNAME]);
+		printf("usage %s: 'Number of elements' 'order'\n", argv[PROGNAME]);
+		printf("Order:\n (0)random\n (1)increasingly\n (2)decreasingly\n");
 		exit(0);
 	}	
 
 	n = atoi(argv[N]);
+	order = atoi(argv[ORDER]);
 
-	int *vector = vector_create_random(n);
+	if(order == 0)
+		vector = vector_create_random(n);
+	else if(order == 1)
+		vector = vector_create_sorted(n);
+	else if(order == 2)
+		vector = vector_create_reversed(n);
+	else{
+		printf("Invalid order option. Exiting program...\n");
+		exit(0);
+	}
+
 	vector_print(vector, n);
 
 	bubble_sort(vector, n);
 
 	vector_print(vector, n);
 
+	printf("Comp = %d\n", comp);
+	printf("Swaps = %d\n", swaps);
 
 
 	return EXIT_SUCCESS;
